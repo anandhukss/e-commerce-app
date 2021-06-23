9b7ff5d5500540645ca7a6b57a250a153d47570e
ks <anandhuks1027@gmail.com>
2021-06-23 18:57:18 +0530

    compoents created,  styiling menu items done

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import './menu-item.styles.scss';
+import React from 'react'
+
+function MenuItem({title,imageUrl,size}) {
+    return (
+        <div  className={`menu-item ${size}`}>
+            <div className="background-image" style={{backgroundImage:`url("${imageUrl}")`}}></div>
+        <div className="content">
+          <h1 className="title">{title}</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    )
+}
+
+export default MenuItem
